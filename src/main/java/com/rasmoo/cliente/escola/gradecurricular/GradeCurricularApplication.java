b91c9ee1d6b3f12697b4c005a9ee9cf974b41a39
Thiago Menezes <thi.menezes14@gmail.com>
2021-05-25 16:41:45 -0300

    Primeiro controller

--- src/main/java/com/rasmoo/cliente/escola/gradecurricular/GradeCurricularApplication.java
+++ src/main/java/com/rasmoo/cliente/escola/gradecurricular/GradeCurricularApplication.java
@@ -2,10 +2,22 @@ package com.rasmoo.cliente.escola.gradecurricular;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/*
+ * CONFIGURAÇÕES DE INICIALIZAÇÃO DE API REST NO SPRINGBOOT:
+ * ESTENDER A CLASSE PARA SPRINGBOOTSERVLETINITIALIZER E SOBRESCREVER SEU MÉTODO CONFIGURE PASSANDO A CLASSE PRINCIPAL DO PROJETO
+ * */
 
 @SpringBootApplication
-public class GradeCurricularApplication {
-
+public class GradeCurricularApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GradeCurricularApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GradeCurricularApplication.class, args);
 	}
